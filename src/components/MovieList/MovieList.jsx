9f35037c5e5a6f70eb817183810209928219a697
Pete Hall <pete.mack.hall@gmail.com>
2022-06-30 21:50:28 -0500

    Added delete button for each movie. Set up connection to return pass movie.id as an argument in the deleteMovie function.

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -14,9 +14,12 @@ function MovieList() {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
+    const deleteMovie = (arg) => {
+        console.log('in deleteMovie:', arg);
+    }
+
     const handleClick = (arg) => { // arg is just a parameter. down below, the movie.id becomes the parameter we're passing in this function
         console.log('in handleClick:', arg); // how to pass an paramenter in the onClick function https://upmostly.com/tutorials/pass-a-parameter-through-onclick-in-react
-        //dispatch({type: 'MOVIE_INFO', payload: arg}); // send this to the root saga to start that flow - don't need this because on page load in details it sends the same dispatch
         history.push(`/details/${arg}`); // send us to the details page for the specific movie we clicked on
         // this also works for params https://stackoverflow.com/questions/40947650/axios-get-in-url-works-but-with-second-parameter-as-object-it-doesnt
     }
@@ -30,6 +33,8 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
+                            <button onClick={() => deleteMovie(movie.id)}>Delete</button>
+                            <br/>
                             <img src={movie.poster} alt={movie.title} onClick={() => handleClick(movie.id)}/>
                         </div>
                     );
